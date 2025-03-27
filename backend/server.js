6825d234b0c470d5fb1dba248a72c0c2f82f3104
jinvkyen <ayenjtt.1902@gmail.com>
2025-03-27 21:51:10 +0800

    server/login and register-done

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import mongoose from "mongoose";
 import "dotenv/config";
 import bcrypt from "bcrypt";
 import User from "./Schema/User.js";
+import { nanoid } from "nanoid";
+import jwt from "jsonwebtoken";
 
 const server = express();
 let PORT = process.env.PORT;
@@ -11,8 +13,32 @@ mongoose.connect(process.env.DB_LOCATION, {
   autoIndex: true,
 });
 
+// register
+
 let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; // regex for email
 let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/; // regex for password
+
+// formatting data to send
+const formatDatatoSend = (user) => {
+  const acces_token = jwt.sign({ id: user._id }, process.env.SECRET_ACCESS_KEY);
+
+  return {
+    acces_token,
+    profile_img: user.personal_info.profile_img,
+    username: user.personal_info.username,
+    fullname: user.personal_info.fullname,
+  };
+};
+
+// generate user function
+const generateUsername = async (email) => {
+  let username = email.split("@")[0];
+
+  let isUsernameNotUnique = await User.exists({ "personal_info.username": username }).then((result) => result);
+
+  isUsernameNotUnique ? (username += nanoid().substring(0, 5)) : "";
+  return username;
+};
 
 // to use json body
 server.use(express.json());
@@ -39,10 +65,9 @@ server.post("/user/register", (req, res) => {
     return res.status(403).json({ error: "Enter a stronger password." });
   }
 
-  bcrypt.hash(password, 10, (hashed_password) => {
+  bcrypt.hash(password, 10, async (err, hashed_password) => {
     try {
-      let username = email.split("@")[0];
-
+      let username = await generateUsername(email);
       let user = new User({
         personal_info: {
           fullname,
@@ -52,16 +77,55 @@ server.post("/user/register", (req, res) => {
         },
       });
 
-      user.save().then((u) => {
-        return res.status(200).json({ user: u });
-      });
-
+      user
+        .save()
+        .then((u) => {
+          return res.status(200).json(formatDatatoSend(u));
+        })
+        .catch((err) => {
+          return res.status(500).json({ error: "Email already exists." });
+        });
     } catch (error) {
+      if (err.code == 11000) {
+        return res.status(500).json({ error: "Email already exists." });
+      }
       return res.status(500).json({ error: error.message });
     }
   });
 
-//   return res.status(200).json({ status: "okay" });
+  //   return res.status(200).json({ status: "okay" });
+});
+
+// sign in
+server.post("/user/login", (req, res) => {
+  let { email, password } = req.body;
+
+  User.findOne({ "personal_info.email": email })
+    .then((user) => {
+      if (!user) {
+        return res.status(403).json({ error: "Email not found." });
+      }
+
+      bcrypt.compare(password, user.personal_info.password, (err, result) => {
+        if (err) {
+          return res.status(403).json({ error: "Error occure while logging in, please try again." });
+        }
+
+        if (!result) {
+          // be vague for security reasons
+          return res.status(403).json({ error: "Incorrect credentials." });
+        } else {
+          return res.status(200).json(formatDatatoSend(user));
+        }
+      });
+
+      // console.log(user);
+      // return res.json({ status: "document was retrieved from the user." });
+    })
+    .catch((err) => {
+      console.log(err.message);
+      return res.status(500).json({ error: err.message });
+    });
 });
 
 // listening
